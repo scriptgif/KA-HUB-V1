42a3ba2d8035d72abcc8cb8636555643602b581f
scriptgif <kaynancardoso216@gmail.com>
2026-01-05 19:17:22 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,7 +4,7 @@ local KeyOficial = "KA-"..os.date("%d%m").."-HUB"
 -- Função que puxa o código principal do seu GitHub
 local function LoadMain()
     -- IMPORTANTE: Troque o link abaixo pelo seu link RAW do source.lua
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/SEU_USUARIO/KA-HUB-V1/main/source.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptgif/KA-HUB-V1/refs/heads/main/Source.lua"))()
 end
 
 -- Aqui você coloca aquela telinha de "Insira a Key" que criamos
